4167438477d38891c5cebbacc53b7a707f010f77
Horacio Valdes <horavaldesg@gmail.com>
2021-01-31 10:49:41 -0500

    public speed

--- Assets/Scripts/Jitter.cs
+++ Assets/Scripts/Jitter.cs
@@ -6,8 +6,9 @@ public class Jitter : MonoBehaviour
 {
     int x = 10;
     int y = 25;
-    int speed = 2;
+    public int speed = 2;
     Vector3 orgPos;
+
     // Start is called before the first frame update
     void Start()
     {
