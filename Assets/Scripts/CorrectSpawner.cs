1c144e24cdc270533838e95186d48c0a1ff0fbe6
Horacio Valdes <horavaldesg@gmail.com>
2021-01-30 19:33:19 -0500

    Movement

--- Assets/Scripts/CorrectSpawner.cs
+++ Assets/Scripts/CorrectSpawner.cs
@@ -7,6 +7,7 @@ public class CorrectSpawner : MonoBehaviour
     int x = 0;
     public GameObject obj;
     float randF;
+    bool oneSpawn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +17,26 @@ public class CorrectSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+           
+        
+
+       
+    }
+    public void OneSpawn()
+    {
+        GameObject spwnObj = Instantiate(obj);
+        spwnObj.transform.position = new Vector3(Random.Range(-3, 3) + randF, Random.Range(4, -2) + randF, 0);
+    }
+    public void MultiSpawn()
+    {
         randF = Random.Range(-0.5f, 0.5f);
-        if (x < 15)
+        for(int i = 0; i < 15; i++)
         {
-            
+
             GameObject spwnObj = Instantiate(obj);
             spwnObj.transform.position = new Vector3(Random.Range(-3, 3) + randF, Random.Range(4, -2) + randF, 0);
-            x++;
+            
         }
     }
 }
