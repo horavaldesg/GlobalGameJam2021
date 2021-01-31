2f86f44265cd58e697d46fe1a44c2816e9fe2137
Horacio Valdes <horavaldesg@gmail.com>
2021-01-31 14:57:20 -0500

    Audio

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class CharacterSelect : MonoBehaviour
 {
     public static Animator anim;
+    public AudioClip correct;
+    public AudioClip incorrect;
+    public AudioClip changeScene;
+    AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,6 +27,8 @@ public class CharacterSelect : MonoBehaviour
         if (gameObject.transform.tag == FindThisSpawner.currentObj)
         {
             //Debug.Log("Correct choice");
+            audio.clip = correct;
+            audio.Play();
             triggerAnim();
             //Time.timeScale = 0;
 
@@ -30,6 +36,8 @@ public class CharacterSelect : MonoBehaviour
         }
         else
         {
+            audio.clip = incorrect;
+            audio.Play();
             Timer.penalty = true;
             //Debug.Log("Wrong Choice");
             Timer.reduceTime -= 2;
@@ -38,6 +46,8 @@ public class CharacterSelect : MonoBehaviour
     }
     public void triggerAnim()
     {
+        audio.clip = changeScene;
+        audio.Play();
         anim.SetTrigger("LevelEnd");
         CorrectSpawner.respawn = true;
     }
